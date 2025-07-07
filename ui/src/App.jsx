939553d553cc9316806f9d80f6418b00fcda7fed
Alejandro Suarez <git@aspcorpo.com>
2025-07-07 15:39:39 -0500

    feat(ui): apply page-container to main app content

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="page-container">
       {user ? (
         <Dashboard user={user} onLogout={handleLogout} />
       ) : (
